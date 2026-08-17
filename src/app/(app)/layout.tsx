722a1b50102595837f3310d56c3a05b5462a9f52
Cristhiangr333 <cristhiangomezromero@gmail.com>
2026-08-17 17:20:10 -0500

    Update layout.tsx

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,5 @@
 import { Sidebar } from "@/components/shared/Sidebar";
 import { TopBar } from "@/components/shared/TopBar";
-import { EdgeCard } from "@/components/shared/EdgeCard";
 import { NavTabs } from "@/components/shared/NavTabs";
 
 export default function AppLayout({
@@ -14,7 +13,6 @@ export default function AppLayout({
       <div className="flex-1 min-w-0">
         <div className="max-w-[1240px] mx-auto px-4 md:px-8 py-6 md:py-7">
           <TopBar />
-          <EdgeCard />
           <NavTabs />
           {children}
         </div>
